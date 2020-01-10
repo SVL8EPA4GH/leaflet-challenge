29408aee4e6cfe376015d4c88f8033e31ec38741
SVL4Data <50878450+SVL8EPA4GH@users.noreply.github.com>
2020-01-10 09:13:52 -0500

    Add files via upload

---
+++ static/js/config_SVL_API.js
@@ -0,0 +1,2 @@
+//SVL's API key for mapbox
+const API_KEY = "pk.eyJ1Ijoic3ZsNG1hcCIsImEiOiJjazQxejFieGQwNnA4M2xvMHV2YThwMTNiIn0.cF09htROCjoVrDxUdfnsYA";
---
+++ static/js/logic_4.js
@@ -0,0 +1,177 @@
+// Store our API endpoint inside queryUrl
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+
+/*
+// Grab the data with d3
+d3.json(url, function(response) {
+
+  // Create a new marker cluster group
+  var markers = L.markerClusterGroup();
+
+  // Loop through data
+  for (var i = 0; i < response.length; i++) {
+
+    // Set the data location property to a variable
+    var location = response[i].location;
+
+    // Check for location property
+    if (location) {
+
+      // Add a new marker to the cluster group and bind a pop-up
+      markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]])
+        .bindPopup(response[i].descriptor));
+    }
+
+  }
+
+  // Add our marker cluster layer to the map
+  myMap.addLayer(markers);
+
+});
+
+/*
+
+
+// Perform a GET request to the query URL
+d3.json(queryUrl, function(data) {
+  // Once we get a response, send the data.features object to the createFeatures function
+  createFeatures(data.features);
+    // Loop through data
+  for (var i = 0; i < data.length; i++) {
+
+    // Set the data location property to a variable
+    var location = data[i].coordinates;
+
+    // Check for location property
+    if (location) {
+
+      // Add a new marker to the cluster group and bind a pop-up
+      markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]])
+        .bindPopup(response[i].mag));
+    }
+
+  }
+});
+
+function createFeatures(earthquakeData) {
+
+  // Define a function we want to run once for each feature in the features array
+  // Give each feature a popup describing the place and time of the earthquake
+  function onEachFeature(feature, layer) {
+    layer.bindPopup("<h3>" + feature.properties.place + "<hr>" + "Magnitute: " + feature.properties.mag +
+      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+  }
+
+  // Create a GeoJSON layer containing the features array on the earthquakeData object
+  // Run the onEachFeature function once for each piece of data in the array
+  var earthquakes = L.geoJSON(earthquakeData, {
+    onEachFeature: onEachFeature
+  });
+
+  // Sending our earthquakes layer to the createMap function
+  createMap(earthquakes);
+}
+
+// Create a new marker cluster group
+var markers = L.markerClusterGroup();
+
+// Function to determine marker size based on earthquake magnitute; cf. 17-1-A09
+function markerSize(mag) {
+  return mag * 40;
+}
+
+// An array containing all of the information needed to create city and state markers
+var loc_mag = [
+  {
+    coordinates: [40.7128, -74.0059],
+    mag: [1]
+  },
+  {
+    coordinates: [34.0522, -118.2437],
+    mag: [2]
+  },
+  {
+    coordinates: [41.8781, -87.6298],
+    mag: [3]
+  },
+  {
+    coordinates: [29.7604, -95.3698],
+    mag: [4]
+  },
+  {
+    coordinates: [41.2524, -95.9980],
+    mag: [5]
+  }
+];
+
+// Define arrays to hold created loc_mag markers
+var magMarker = [];
+
+// Loop through loc_mag markers
+for (var i = 0; i < loc_mag.length; i++) {
+
+  // Setting the marker radius for the loc_mag by passing magnitute into the markerSize function
+  magMarkers.push(
+    L.circle(loc_mag[i].coordinates, {
+      stroke: false,
+      fillOpacity: 0.75,
+      color: "purple",
+      fillColor: "purple",
+      radius: markerSize(loc_mag[i].mag)
+    })
+  );
+}
+
+function createMap(earthquakes) {
+
+  // Define streetmap and darkmap layers
+  var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken: API_KEY
+  });
+
+  var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.dark",
+    accessToken: API_KEY
+  });
+
+  // Define a baseMaps object to hold our base layers
+  var baseMaps = {
+    "Street Map": streetmap,
+    "Dark Map": darkmap
+  };
+
+  // Create two separate layer groups: one for earthquaker location and one for earthquaker magnitute; cf.17-1A09
+var location = L.layerGroup(locationMarkers);
+var magnitute = L.layerGroup(magnitudeMarkers);
+
+
+
+  // Create overlay object to hold our earthquaker overlay layer
+  var overlayMaps = {
+    Earthquakes: earthquakes
+    Earthquakes: magnitute
+  };
+
+
+  // Create our map, giving it the streetmap and earthquakes layers to display on load
+  var myMap = L.map("map", {
+    center: [
+      37.09, -95.71
+    ],
+    zoom: 5,
+    layers: [streetmap, earthquakes]
+  });
+
+  // Create a layer control
+  // Pass in our baseMaps and overlayMaps
+  // Add the layer control to the map
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+}
